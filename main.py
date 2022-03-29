9d661624a442499369bbcf48ede38404b2d9ceaa
sn00wden <matthieu2603@gmail.com>
2022-03-29 15:56:45 +0200

    First final version (need opti)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import csv
 import random
 import copy
+import matplotlib.pyplot as plt
 
 def ExtractFile(path) :
     
@@ -50,8 +51,10 @@ class Unit :
             results[i][0],results[i+1][0] = results[i][0][:int(len(results[i][0])/2):] + results[i+1][0][int(len(results[i][0])/2)::],results[i+1][0][:int(len(results[i][0])/2):] + results[i][0][int(len(results[i][0])/2)::]
             results[i][1] = self.Fitness(results[i][0])
             results[i+1][1] = self.Fitness(results[i+1][0])
-        
-        return results.sort(key=lambda x : x[1], reverse = False)
+
+        results.sort(key=lambda x : x[1], reverse = False)
+
+        return results
 
     def Equal(results):
         # temp = True
@@ -71,23 +74,30 @@ class Unit :
     def Mutations(self, save_result : list):
         results = copy.deepcopy(save_result)
 
-        for i in results :
-            temp = int(random.randint(0,len(i[0])))
-            for j in range(temp,int(random.randint(0,len(i[0])))):
-                i[0][j]+=  random.uniform(-50,50) #Change values random.uniform(a,b) if you want a wider mutation
-            i[1] = self.Fitness(i[0])
-        return results.sort(key=lambda x : x[1], reverse = False)
+        for i in range(len(save_result)) :
+            temp = int(random.randint(0,len(save_result[i][0])))
+            for j in range(temp,int(random.randint(temp,len(save_result[i][0])))):
+                results[i][0][j] =  random.uniform(-100,100) #Change values random.uniform(a,b) if you want a wider mutation
+            results[i][1] = self.Fitness(results[i][0])
+        results.sort(key=lambda x : x[1], reverse = False)
+
+        return results
 
     def Compare(save_result : list, temp : list):
-        for i in range(len(save_result)) :
 
-            if(save_result[i][1]>temp[i][1]) :
-                save_result.insert(i)
-            
+        result = copy.deepcopy(save_result)
+        result.extend(x for x in temp if x not in result)
+        # result = result + list(set(temp)-set(result))
+        
+        result.sort(key=lambda x : x[1], reverse = False)
+
+        result = result[:len(save_result)]
+        save_result.clear()
+        save_result.extend(result)
                 
                 
         
-        return save_result
+        return result
 
     def Iterate(self,it):
         save_result=[]
@@ -113,34 +123,38 @@ class Unit :
         counter = 0
         temp_bool = False
         try : 
-            while not temp_bool :
+            while save_result[0][1] > 10:
 
+                save_result_temp.clear()
                 
-                # save_result_temp = self.Iterate(it_bygen)
+                save_result_temp.extend(self.Iterate(it_bygen))
    
                 
-                # Unit.Compare(save_result, save_result_temp)
-                
-                
-                # save_result_temp = self.Mutations(save_result)
-                
-                # Unit.Compare(save_result, save_result_temp)
-                
-                print(1)
-                temp_bool = Unit.Equal(save_result)
-                save_result_temp = self.Cross(save_result)
-                print(2)
-                temp_bool = Unit.Equal(save_result)
                 Unit.Compare(save_result, save_result_temp)
-                print(3)
-                temp_bool = Unit.Equal(save_result)
+                
+                
+                save_result_temp.clear()
+                save_result_temp.extend(self.Mutations(save_result))
+                # print("C : ",save_result_temp)
+                
+                Unit.Compare(save_result, save_result_temp)
+                # print("2 :", save_result_temp)
+                
+                # temp_bool = Unit.Equal(save_result)
+                save_result_temp.clear()
+                save_result_temp.extend(self.Cross(save_result))
+                Unit.Compare(save_result, save_result_temp)
 
-                if counter == 1000:
+
+
+
+                if counter == 50:
                     # print("temp",save_result[0][0],save_result[0][1])
-                    print("temp",save_result)
+                    print("temp",save_result[0][1])
                     print("Avg :",Unit.Avg_Fitness(save_result))
                     counter = 0
                 counter += 1
+            return save_result[0]
         except KeyboardInterrupt:
             return save_result[0]
         
@@ -166,17 +180,25 @@ if __name__ == "__main__":
     # print(extract)
     IA = Unit(extract)
     # print(IA.Fitness([50,4,5,60,1,2]))
-    result = IA.Generation(20)
+    result = IA.Generation(50)
     print("result :",result)
+    for i in range(len(extract)):
+        plt.scatter(extract[i][1],extract[i][2],color ='red')
+        plt.scatter(result[0][0]* np.sin(result[0][1]*extract[i][0] + result[0][2]),result[0][3]* np.sin(result[0][4]*extract[i][0] + result[0][5]),color='blue' )
+    plt.show()
     # print(Unit.Equal([[[1,2,3],1], [[1,2,3],2]]))
-    # temp = [[[2,4,6,10,11,23],10],[[5,7,8,9,10,1],15]]
+    # temp = [[[2,-4,6,10,11,23],10],[[5,7,8,-9,10,1],15],[[3,2,1,-32,-12,16],20]]
+    # while True:
+    #     temp = IA.Mutations(temp)
+    #     print("temp ", temp)
+    # temp2 = [[[],3],[[],5],[[],20]]
     # temp = IA.Iterate(5)
     # print(temp)
     # print(Unit.Avg_Fitness(temp))
 
     # print(IA.Cross(temp))
     # Affiche(temp)
-    # print(Unit.Compare(temp,[[[5,10,15,20,25],5],[[],20]]))
+    # print(Unit.Compare(temp,temp2))
     # print(temp)
     # print(IA.Iterate())
     pass
